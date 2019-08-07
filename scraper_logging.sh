59627129b90c26ca92cc6f57f67bea7dd8b8126d
Rebecca V <rebeccarvickers@gmail.com>
2019-08-07 17:03:16 -0500

    endpoints updated to heroku dyno

--- scraper_logging.sh
+++ scraper_logging.sh
@@ -10,7 +10,7 @@ done
 START_TIME=$(date +"%Y-%m-%d %T")
 
 # Send start time and scraper name to db and get id of new row.
-ROW_ID=$(curl -H "Content-Type: application/json" -X POST -d '{"start_time": "'"$START_TIME"'", "name": "'"$SCRAPER_NAME"'"}' http://localhost:3000/start)
+ROW_ID=$(curl -H "Content-Type: application/json" -X POST -d '{"start_time": "'"$START_TIME"'", "name": "'"$SCRAPER_NAME"'"}' https://scraper-logger.herokuapp.com/start)
 
 # Status will be the final line of stdout+stderr.  If the scraper completed,
 # final line should be 'COMPLETE'. Else final line should be from stderr.
@@ -19,4 +19,4 @@ STATUS=$(COMMAND $RUN_SCRAPER 2>&1 | tail -1)
 END_TIME=$(date +"%Y-%m-%d %T")
 
 # Update row with end time and status.
-curl -H "Content-Type: application/json" -X POST -d '{"end_time":"'"$END_TIME"'","status":"'"$STATUS"'","id":"'"$ROW_ID"'"}' http://localhost:3000/end
+curl -H "Content-Type: application/json" -X POST -d '{"end_time":"'"$END_TIME"'","status":"'"$STATUS"'","id":"'"$ROW_ID"'"}' https://scraper-logger.herokuapp.com/end
