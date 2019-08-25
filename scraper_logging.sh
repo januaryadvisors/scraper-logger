857ed780eaf8c99c8df350b44a631f3775ad3c52
Rebecca V <rebeccarvickers@gmail.com>
2019-08-24 17:22:06 -0700

    commented-out text removed

--- scraper_logging.sh
+++ scraper_logging.sh
@@ -23,14 +23,7 @@ then
 	STATUS='ERROR'
 fi
 
-# LOGURL=$(curl -X POST -H "Content-Type: application/x-www-form-urlencoded; charset=utf-8" -d "api_paste_code=$FULLLOG&api_option=paste&api_paste_private=1&api_paste_name='$SCRAPER_NAME $START_TIME'&api_dev_key=93223c64ad71317e136af2f10d505eae" https://pastebin.com/api/api_post.php)
-
 END_TIME=$(date +"%Y-%m-%d %T")
 
 # Update row with end time and status.
 curl -H "Content-Type: application/x-www-form-urlencoded" -X POST -d "end_time=$END_TIME&status=$STATUS&full_log=$FULLLOG&id=$ROW_ID" https://scraper-logger.herokuapp.com/end
-
-# echo $PASTEBINKEY
-
-
-# echo $LOGURL
